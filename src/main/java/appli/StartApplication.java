cb40ff03d5d607458722f4b9b7e5d84358475315
Hdidogs <a.rohee@lprs.fr>
2025-01-27 14:00:11 +0100

    Fix patch bug star appli & feat: add add fonction change scene

---
+++ src/main/java/appli/StartApplication.java
@@ -0,0 +1,46 @@
+package appli;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartApplication extends Application {
+    private static Stage main;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        main = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 760);
+        main.setScene(scene);
+        main.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public static void changeScene(String nomDuFichierFxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 760);
+        main.setScene(scene);
+        main.show();
+    }
+
+    public static void changeScene(String nomDuFichierFxml, Object controller) {
+        main.close();
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml + ".fxml"));
+            fxmlLoader.setController(controller);
+            Scene scene = new Scene(fxmlLoader.load());
+            main.setScene(scene);
+            main.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
